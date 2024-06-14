10b797972a0ac73e0885d15653c9231603ef0d3c
MUHULILAMRI <105841106221@student.unismuh.ac.id>
2024-06-15 00:21:39 +0800

    Button

--- App.js
+++ App.js
@@ -5,45 +5,54 @@ import { setStatusBarBackgroundColor } from 'expo-status-bar'
 const App = () => {
   return (
     <View style={{
-      flex: 1, 
-      marginTop: 300,
+      flex: 1,
+      flexDirection: 'row'
+      
     }}>
-     <View style={{
-        // flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
       <View style={{
-        width: 100, 
-        height: 100,
-        backgroundColor: 'red'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginLeft: 70,
       }}>
-      </View>
-      </View>
         <View style={{
-          // flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: 100,
+          height: 50,
+          backgroundColor: 'red',
+          borderRadius:10,
+          justifyContent: 'center'
         }}>
-          <View style={{
-            width: 100,
-            height: 100,
-            backgroundColor: 'blue'
-          }}>
-            <Text style={{
-              fontSize: 34,
-              color: 'white',
-              textAlign: 'center',
-              fontWeight: 'bold'
-            }}>
-            </Text>
-          </View>
+          <Text style={{
+            fontsize:20,
+            color: 'white',
+            textAlign: 'center',
+            fontWeight: 'bold',
+          }}>LOGIN</Text>
         </View>
-
       </View>
-    
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginRight: 70,
+      }}>
+        <View style={{
+          width: 100,
+          height: 50,
+          backgroundColor: 'blue',
+          borderRadius: 10,
+          justifyContent: 'center'
+        }}>
+          <Text style={{
+            fontzise: 20,
+            color: 'white',
+            textAlign: 'center',
+            fontWeight: 'bold',
+          }}>SingUp</Text>
+        </View>
+      </View>
+    </View>
   )
-  
 }
    
 export default App
