629a4d56363eb9de2c30afe751106da8829079b4
MUHULILAMRI <105841106221@student.unismuh.ac.id>
2024-06-25 00:22:27 +0800

    TextInputCostum

--- App.js
+++ App.js
@@ -1,44 +1,38 @@
-import React from 'eact';
-import { View, Text } from 'eact-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
 
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'ed',
-    width: 250,
-    height: 100,
-    borderRadius: 10,
-    justifyContent: 'center',
-    marginBottom: 10,
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: 'white',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-};
-
-const ButtonCustom = ({ text, color }) => {
-  return (
-    <View style={[styles.button, { backgroundColor: color }]}>
-      <Text style={styles.buttonText}>{text}</Text>
-    </View>
-  );
-};
+const TextInputCostum =({placeholder, color, typeKeybooard}) => {
+  return(
+    <TextInput  
+    placeholder={placeholder}
+    keyboardType={typeKeybooard}
+    style ={{
+      width :250,
+      height : 50,
+      borderColor : color,
+      borderWidth : 1,
+      borderRadius : 10,
+      marginBottom :10,
+      paddingLeft : 10
+    }}></TextInput>
+  )
+}
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <ButtonCustom text="Login" color="red" />
-      <ButtonCustom text="Register" color="blue" />
-      <ButtonCustom text="Register" color="green" />
+    <View style ={{
+      flex : 1,
+      justifyContent : 'center',
+      alignItems : 'center',
+    }}>
+      <TextInputCostum placeholder = "Username" color = "black" />
+      <TextInputCostum placeholder = "Password" color = "red" />
+      <TextInputCostum placeholder = "Gmail" color = "red" />
+      {/* <ButtonCostum text = "LOGIN" color = "red" />
+      <ButtonCostum text = "LOGIN" color = "orange" />
+      <ButtonCostum text = "REGIS" color = "purple" /> */}
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
